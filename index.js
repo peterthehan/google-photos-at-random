9e465ac9f376a1b7a9a372be54baafd68032424a
Peter Han <peter.han.9493@gmail.com>
2020-07-11 12:40:46 -0700

    Handle cli args in main

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const { albumId } = require("./config");
 const main = async (auth) => {
   const photos = new Photos(auth.credentials.access_token);
 
-  // await getAllPhotoIds(photos, albumId);
-  await getRandomPhotos(photos, 5);
+  const [, , ...args] = process.argv;
+  switch (args[0]) {
+    case "--load":
+      return await getAllPhotoIds(photos, albumId);
+    case "--choose":
+      return await getRandomPhotos(photos, 10);
+  }
 };
 
 authenticate().then(main).catch(console.error);
